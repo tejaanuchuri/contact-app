4c3d631c12611e976f8a8a80e1f90b1a1baa74dc
tejaanuchuri0009 <tejaanuchuri0009@gmail.com>
2020-07-11 18:52:58 +0530

    re create our contact list in app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 
 class ContactList extends React.Component {
   render() {
-    const people = this.props.contacts
+    const people = [
+      { name : "teja" },
+      { name : "Suresh" },
+      { name : "Gayathri" }
+    ]
 
     return <ol>
       {people.map((person) => (
@@ -15,6 +19,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <ContactList />
+        <ContactList />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,24 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-class ContactList extends React.Component {
-  render() {
-    const people = [
-      { name : "teja" },
-      { name : "Suresh" },
-      { name : "Gayathri" }
-    ]
 
-    return <ol>
-      {people.map((person) => (
-        <li key={person.name}>{person.name}</li>
-      ))}
-    </ol>
-  }
-}
 
 ReactDOM.render(
-  <ContactList />,
+  <App />,
   document.getElementById('root')
 );
 
